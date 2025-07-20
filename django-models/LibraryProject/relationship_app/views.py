c92336f68927fdc05d4a6f5fdc6a0b2cfec03a3b
Paschal Ebitse <solidpaska@gmail.com>
2025-07-20 21:50:16 +0100

    New commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 
 # Function-based view to list all books
