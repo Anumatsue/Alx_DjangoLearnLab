db9be6be77477aca2342a5ef122e47565501a60e
Paschal Ebitse <solidpaska@gmail.com>
2025-07-27 14:25:51 +0100

    New Commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-from .new_views.admin_view import admin_view
-from .new_views.librarian_view import librarian_view
-from .new_views.member_view import member_view
+from .Admin.admin_view import admin_view
+from .Librarian.librarian_view import librarian_view
+from .Member.member_view import member_view
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
@@ -15,7 +15,7 @@ urlpatterns = [
 
 urlpatterns = [
     path('librarian-role/', librarian_view, name='librarian_view'),
-    path('admin-page/', admin_view, name='admin_view'),
+    path('admin-role/', admin_view, name='admin_view'),
     path('member-role/', member_view, name='member_view'),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
