83700e7baf7b1666efe33b890c8f7e6edd31398d
Paschal Ebitse <solidpaska@gmail.com>
2025-08-03 20:26:46 +0100

    New Commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny] # Optional: open access for this view
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Secured
