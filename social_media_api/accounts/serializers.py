2cc095882700a2199c1182cc60322cf57bf74734
Paschal Ebitse <solidpaska@gmail.com>
2025-08-24 23:52:55 +0100

    New Features

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -10,14 +10,16 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "email", "bio", "profile_picture"]
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # <-- explicitly use CharField
+    # ✅ Explicitly declare password with serializers.CharField()
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
         fields = ["username", "email", "password"]
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(  # <-- use get_user_model()
+        # ✅ Use get_user_model().objects.create_user
+        user = get_user_model().objects.create_user(
             username=validated_data["username"],
             email=validated_data["email"],
             password=validated_data["password"],
